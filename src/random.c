caf87ebea4587497ea74eeb835c8e1fc8bbfea05
draradech <kasten.m@gmx.de>
2022-06-03 21:50:49 +0200

    add xorshf96, much faster than rand()

--- src/random.c
+++ src/random.c
@@ -39,3 +39,20 @@ uint randn(uint n) {
 
 	return r % n;
 }
+
+//period 2^96-1
+unsigned long xorshf96(void) {
+	static unsigned long x=123456789, y=362436069, z=521288629;
+	unsigned long t;
+  x ^= x << 16;
+  x ^= x >> 5;
+  x ^= x << 1;
+
+	t = x;
+	x = y;
+	y = z;
+	z = t ^ x ^ y;
+
+  return z;
+}
+
--- src/random.h
+++ src/random.h
@@ -1,2 +1,3 @@
 extern void random_seed(void);
 extern unsigned int randn(unsigned int n);
+extern unsigned long xorshf96(void);
